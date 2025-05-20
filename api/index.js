d95d2282a446f9ecd730e4c92848c19c2629c1b0
TouseefAhmadO <ta8946952@gmail.com>
2025-05-20 23:36:20 +0500

    update the code

--- api/index.js
+++ api/index.js
@@ -1,23 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./middleware/connectDB");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
+
+// Routes
 app.use("/api/auth", require("../routes/auth"));
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
+// Export as serverless function
+const serverless = require("serverless-http");
+module.exports.handler = serverless(app);
