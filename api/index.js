a9f8211d566f5369648ae1a629df0ea1ac4312f8
TouseefAhmadO <ta8946952@gmail.com>
2025-05-21 11:49:44 +0500

    change to first commit

--- api/index.js
+++ api/index.js
@@ -7,29 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", require("./routes/auth"));
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", require("../routes/auth"));
 
 mongoose
   .connect(process.env.MONGO_URI, {
